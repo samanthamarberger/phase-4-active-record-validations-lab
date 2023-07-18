f57972ce3e247b9abc55d5e1ef91d3ec4034107e
samanthamarberger <slatham04@gmail.com>
2023-07-18 11:51:32 -0600

    commit tree 3447906b116a1907ce772206d8235ccca9974060

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :clickbait_y
+
+    def clickbait_y
+        clickbait_patterns = [
+            /Won't Believe/i,
+            /Secret/i,
+            /Top \d+/i,
+            /Guess/i
+        ]
+
+        unless clickbait_patterns.any? { |pattern| pattern.match?(title) }
+            errors.add(:title, "is not clickbait-y enough!")
+        end
+    end
+    
 end
